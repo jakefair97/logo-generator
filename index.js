86b80f699e41784149ef135e3271fee84c589d10
jakefair <100549835+jakefair97@users.noreply.github.com>
2023-04-17 18:56:02 -0500

    classes created for different shapes in shapes.js

--- index.js
+++ index.js
@@ -12,19 +12,45 @@ inquirer
         },
         {
             type: 'input',
-            name: 'shape-color',
+            name: 'shapeColor',
             message: 'What color would you like your shape to be? (You may enter a word or a hexadecimal number',
+            default: 'black',
         },
         {
             type: 'input',
             name: 'text',
             message: 'Enter up to 3 characters to have on your logo:',
+            default: '',
         },
         {
             type: 'input',
-            name: 'text-color',
+            name: 'textColor',
             message: 'What color would you like your text to be? (You may enter a word or a hexadecimal number',
+            default: 'white',
         },
-        
-
     ])
+    .then((answers) => {
+        const { shape, shapeColor, text, textColor } = answers;
+
+        let result;
+        let shapeSvg;
+        switch (shape) {
+            case 'Circle':
+                result = new shapes.Circle(shapeColor);
+                shapeSvg = result.render();
+                break;
+            case 'Square':
+                result = new shapes.Square(shapeColor);
+                shapeSvg = result.render();
+                break;
+            case 'Triangle':
+                result = new shapes.Triangle(shapeColor);
+                shapeSvg = result.render();
+                break;
+        }
+
+        let textSvg = text 
+        ? `<text x="50" y="50" text-anchor="middle" fill="${textColor}">${text}</text>`
+        : '';
+
+    })
